13b41761ac243c86c705909663a868e535cb6888
gauravjain-29 <gauravjain29@hotmail.com>
2017-10-20 21:59:39 +0530

    update js

--- sendmailapi.js
+++ sendmailapi.js
@@ -3,20 +3,10 @@ var app = express();
 var fs = require("fs");
 
 app.get('/listUsers', function (req, res) {
-
-
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SendGrid_API_Key);
-const msg = {
-  to: 'gauravjain.sfdev@gmail.com',
-  from: 'test@example.com',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-sgMail.send(msg);
-
-       res.end( 'Success' );
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
 })
 
 var server = app.listen(8081, function () {
