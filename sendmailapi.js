5c7dbe799eea40296454b5030b5108c32db3937e
gauravjain-29 <gauravjain29@hotmail.com>
2017-10-21 00:59:01 +0530

    Sendgrid Email API

--- sendmailapi.js
+++ sendmailapi.js
@@ -5,18 +5,18 @@ var fs = require("fs");
 app.get('/listUsers', function (req, res) {
 console.log('In here');
 res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
-//const sgMail = require('@sendgrid/mail');
-//sgMail.setApiKey(process.env.SendGrid_API_Key);
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SendGrid_API_Key);
 console.log('here');
 console.log(process.env.SendGrid_API_Key);
-//const msg = {
-//  to: 'gauravjain.sfdev@gmail.com',
-//  from: 'test@example.com',
-//  subject: 'Sending with SendGrid is Fun',
-//  text: 'and easy to do anywhere, even with Node.js',
-//  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-//};
-//sgMail.send(msg);
+const msg = {
+  to: 'gauravjain.sfdev@gmail.com',
+  from: 'test@example.com',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
        res.end( 'SUCCESS' );
 })
 app.listen(process.env.PORT || 3000);
